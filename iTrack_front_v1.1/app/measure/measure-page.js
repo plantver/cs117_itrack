ddc1a5a30b0bd766553f2ec1b50cef2b1a2bd274
yufeng <carnate@gmail.com>
2018-08-17 17:42:18 -0700

    more measure changes

--- iTrack_front_v1.1/app/measure/measure-page.js
+++ iTrack_front_v1.1/app/measure/measure-page.js
@@ -9,6 +9,8 @@ wifi_service.setWifiEnabled(true);
 function onNavigatingTo(args) {
     const page = args.object;
 
+    const page = args.object;
+
     var hasPermission = android.os.Build.VERSION.SDK_INT < 23;
     console.log("hasPermission: ", hasPermission)
     if (!hasPermission) {
@@ -22,7 +24,7 @@ function onNavigatingTo(args) {
                         app.android.foregroundActivity,
                         [android.Manifest.permission.ACCESS_COARSE_LOCATION],
                         ACCESS_COARSE_LOCATION_PERMISSION_REQUEST_CODE);
-    }  
+    }
 
     var rs = wifi_service.startScan();
     app.android.unregisterBroadcastReceiver(android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
@@ -31,6 +33,38 @@ function onNavigatingTo(args) {
             , function onReceiveCallback(context, intent) {    
                 var tp = wifi_service.getScanResults();
                 console.log(tp);
+            })
+    app.android.unregisterBroadcastReceiver(android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+    app.android.registerBroadcastReceiver(
+            android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION
+            , function onReceiveCallback(context, intent) {    
+                let tp = java.util.List;
+                tp = wifi_service.getScanResults();
+                tp = tp.toArray()
+                var res = []
+                for (var i = 0; i < tp.length; i++){
+                    var e = tp[i]
+                    info = {"SSID": e.SSID,
+                            "BSSID": e.BSSID,
+                            "RSSI": e.level};
+                    res.push(info);
+                }
+                console.log(res)
+                console.log('http://13.57.182.179:5001/locate');
+                httpModule.request({
+                    url: 'http://13.57.182.179:5001/locate',
+                    method: 'POST',
+                    headers: {"Content-Type": "application/json"},
+                    content: JSON.stringify(res)
+                }).then((response) => {
+                    console.log(response.content);
+                    var res = response.content.toJSON();
+                    if (response.statusCode == 200){
+                        page.bindingContext.loc = 
+                            {room: res.location.toString(), 
+                            conf: res.relative_probability.toString()}
+                    }
+                }, (e) => {console.log(e)});
             })
 
     page.bindingContext = new BrowseViewModel();
@@ -41,5 +75,22 @@ function onDrawerButtonTap(args) {
     sideDrawer.showDrawer();
 }
 
+function onMeasureTap(args)
+{
+    const button = args.object
+    var rs = wifi_service.startScan();
+}
+
+function onUploadTap(args)
+{
+    const button = args.object
+    var rs = wifi_service.startScan();
+}
+
+function onReturnPress(args)
+{
+    console.log(args)
+    var roomName = args.object
+}
 exports.onNavigatingTo = onNavigatingTo;
 exports.onDrawerButtonTap = onDrawerButtonTap;
